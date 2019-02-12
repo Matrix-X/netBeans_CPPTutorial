acda298488d42f9c572c1a35b2ae7b4079a44afe
hbj418 <hbj418@gmail.com>
2019-02-12 16:45:02 +0800

    input number and calculator

--- main.cpp
+++ main.cpp
@@ -35,7 +35,34 @@ const double PI = 3.14159;
  * argv : point to whole bunch of arguments value
  */
 int main(int argc, char** argv) {
-
+    
+    
+    std::string sMiles;
+    double dMiles, dKilometers;
+    std::cout << "Enter Miles : ";
+    getline(std::cin, sMiles);
+    dMiles = std::stod(sMiles);
+    dKilometers = dMiles * 1.60934;
+    printf("%.1f miles equals %.4f kilometers \n",dMiles, dKilometers);
+    
+    
+//    -------------------------------------------------------
+//    std::string sQuestion ("Enter Number 1: ");
+//    std::string sNum1, sNum2;
+//    std::cout << sQuestion;
+//    getline(std::cin, sNum1);
+//    std::cout << "Enter Number 2: ";
+//    getline(std::cin, sNum2);
+//    
+//    int nNum1 = std::stoi(sNum1);
+//    int nNum2 = std::stoi(sNum2);
+//    
+//    printf("%d + %d = %d \n", nNum1, nNum2, (nNum1 + nNum2));
+//    printf("%d - %d = %d \n", nNum1, nNum2, (nNum1 - nNum2));
+//    printf("%d * %d = %d \n", nNum1, nNum2, (nNum1 * nNum2));
+//    printf("%d / %d = %d \n", nNum1, nNum2, (nNum1 / nNum2));
+//    printf("%d %% %d = %d \n", nNum1, nNum2, (nNum1 % nNum2));
+    
 //    -------------------------------------------------------
     
 //    double dbBigFloat = 1.11111111111111111111;
