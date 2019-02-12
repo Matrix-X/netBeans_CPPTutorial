4b74aafb7f6acb525117fd13d4949e54cd9a9719
hbj418 <hbj418@gmail.com>
2019-02-12 15:56:02 +0800

    first helloworld output with arguments

--- main.cpp
+++ main.cpp
@@ -16,14 +16,31 @@
 // which is loading files and objects 
 // contains pre-made functions
 #include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector> // resizable arrays
+#include <sstream>  // manipulate strings
 
-using namespace std;
+//using namespace std;
+// using std::out;
 
 /*
- * 
+ * args : arguments count
+ * argv : point to whole bunch of arguments value
  */
 int main(int argc, char** argv) {
 
+    // std represent your console on your screen
+    std::cout << "Hello world!" << std::endl;
+    
+    if(argc != 1){
+        std::cout << "You entered " << argc << "arguments \n" << std::endl;
+    }
+    
+    for(int i=0; i<argc; ++i){
+        std::cout << "No:" << i << " value is :" << argv[i] << " \n" << std::endl;
+    }
+    
     return 0;
 }
 
