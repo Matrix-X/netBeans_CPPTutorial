f844eb3b6148e536597e700d4586f9d7dcf48866
hbj418 <hbj418@gmail.com>
2019-02-12 17:55:27 +0800

    using vector for calculator

--- main.cpp
+++ main.cpp
@@ -39,9 +39,36 @@ const double PI = 3.14159;
  */
 int main(int argc, char** argv) {
 
+    double dbNum1 = 0, dbNum2 = 0;
+    std::string sCalc = "";
+    std::vector<std::string> vectCalc;
     
+    std::cout << "Enter calculation (ex. 5 + 6) : ";
+    getline(std::cin, sCalc);
+    std::stringstream ss(sCalc);
+    std::string sIndivStr;
+    char cSpace = ' ';
 
+    while(getline(ss, sIndivStr, cSpace)){
+        vectCalc.push_back(sIndivStr);
+    }
 
+    dbNum1 = std::stoi(vectCalc[0]);
+    dbNum2 = std::stoi(vectCalc[2]);
+
+    std::string operation = vectCalc[1];
+
+    if(operation == "+"){
+        printf("%.1f + %.1f = %.1f \n", dbNum1, dbNum2, dbNum1+dbNum2);
+    }else if(operation == "-"){
+        printf("%.1f - %.1f = %.1f \n", dbNum1, dbNum2, dbNum1-dbNum2);
+    }else if(operation == "*"){
+        printf("%.1f * %.1f = %.1f \n", dbNum1, dbNum2, dbNum1*dbNum2);
+    }else if(operation == "/"){
+        printf("%.1f / %.1f = %.1f \n", dbNum1, dbNum2, dbNum1/dbNum2);
+    }else{
+        std::cout << "Please enter only +. -, *, /\n";
+    }
 
 //    -------------------------------------------------------
 
