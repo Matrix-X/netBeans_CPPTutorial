758eec556c75ef8afaba9657c4c50412267879af
hbj418 <hbj418@gmail.com>
2019-02-12 15:25:24 +0800

    init project

---
+++ main.cpp
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: michaelhu
+ *
+ * Created on February 12, 2019, 3:13 PM
+ */
+
+
+// this is the c preprocessor directive 
+// which is loading files and objects 
+// contains pre-made functions
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
