f1e6a289cd53fc224560b257f3d80bf4610abc79
hbj418 <hbj418@gmail.com>
2019-02-19 16:44:31 +0800

    Operator Overloading & File I/O

--- main.cpp
+++ main.cpp
@@ -24,6 +24,7 @@
 #include <numeric>
 #include <ctime>
 #include <cmath>
+#include <fstream>
 
 //using namespace std;
 // using std::cout;
@@ -343,6 +344,100 @@ void ShowArea(Shape& shape){
 
 // ------------------------------------------------------------------------------------
 
+class Box {
+public:
+    double length, width, breadth;
+    std::string boxString;
+
+    Box(){
+        length = 1, width = 1, breadth = 0;
+    }
+    Box(double l, double w, double b){
+        length = l, width = w, breadth = b;
+    }
+    Box& operator ++ (){
+        length++;
+        width++;
+        breadth++;
+
+        return *this;
+    }
+
+    operator const char*(){
+        std::ostringstream boxStream;
+        boxStream << "Box : " << 
+                length << ", " <<
+                width << ", " <<
+                breadth;
+        boxString = boxStream.str();
+        return boxString.c_str();
+    }
+
+    Box operator + (const Box& box2){
+        Box boxSum;
+        boxSum.length = length + box2.length;
+        boxSum.width = width + box2.width;
+        boxSum.breadth = breadth + box2.breadth;
+
+        return boxSum;
+    }
+
+    double operator [] (int x){
+        if(x==0){
+            return length;
+        }else if(x==1){
+            return width;
+        }else if(x==2){
+            return breadth;
+        }else{
+            return 0;
+        }
+    }
+
+    bool operator == (const Box& box2){
+        return ((length == box2.length) && 
+                (width == box2.width) &&
+                (breadth == box2.breadth));
+    }
+
+    bool operator < (const Box& box2){
+        double thisSize = length + width + breadth;
+        double box2Size = box2.length + box2.width + box2.breadth;
+        if(thisSize < box2Size){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    bool operator > (const Box& box2){
+        double thisSize = length + width + breadth;
+        double box2Size = box2.length + box2.width + box2.breadth;
+        if(thisSize > box2Size){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    void operator = (const Box& boxToCopy){
+        length = boxToCopy.length;
+        width = boxToCopy.width;
+        breadth = boxToCopy.breadth;
+
+    }
+
+};
+
+
+// ------------------------------------------------------------------------------------
+
+
+
+
+
+// ------------------------------------------------------------------------------------
+
 /*
  * args : arguments count
  * argv : point to whole bunch of arguments value
@@ -350,17 +445,68 @@ void ShowArea(Shape& shape){
 int main(int argc, char** argv) {
 
 //    -------------------------------------------------------
+    std::ofstream writeToFile;
+    std::ifstream readFromFile;
+    std::string txtToWrite = "";
+    std::string txtFromFile = "";
+
+    writeToFile.open("test.txt", std::ios_base::out | std::ios_base::trunc);
+    if(writeToFile.is_open()){
+        writeToFile << "Beginning of File \n";
+        std::cout << "Enter data to write : ";
+        getline(std::cin, txtToWrite);
+        writeToFile << txtToWrite;
+        writeToFile.close();
+    }
+
+    readFromFile.open("test.txt", std::ios_base::in);
+    if(readFromFile.is_open()){
+        while(readFromFile.good()){
+            getline(readFromFile, txtFromFile);
+            std::cout << txtFromFile << std::endl;
+
+            std::vector<std::string> vect = StringToVector(txtFromFile, ' ');
+            int wordsInLine = vect.size();
+            std::cout << "Words in line : " << wordsInLine << "\n";
+            int charCount = 0;
+            for(auto word: vect){
+                for(auto letter: word){
+                    charCount++;
+                }
+            }
+            int avgNumChars = charCount/wordsInLine;
+            std::cout<< "Avg Word length : " <<
+                    avgNumChars << std::endl;
+
+
+        }
+        readFromFile.close();
+    }
 
 
 //    -------------------------------------------------------
 
+    // Box box(10,10,10);
+    // ++box;
+    // std::cout << box << "\n";
+    // Box box2(5,5,5);
+    // std::cout << "Box1 + Box2 = " << box + box2 << "\n";
+    // std::cout << "Box length : " << box[0] << "\n";
+    // std::cout << std::boolalpha;
+    // std::cout << "Are boxes equal :" << 
+    //             (box == box2 ) << "\n";
+    // std::cout << "Is box < box2 : " << (box < box2) << "\n";
+    // std::cout << "Is box > box2 : " << (box > box2) << "\n";
+    
+    // box = box2;
+    // std::cout << "Is box == box2 : " << (box == box2) << "\n";
 
 //    -------------------------------------------------------
 
-    Rectangle rectangle(10,5);
-    Circle circle(10);
-    ShowArea(rectangle);
-    ShowArea(circle);
+    // Rectangle rectangle(10,5);
+    // Circle circle(10);
+    // ShowArea(rectangle);
+    // ShowArea(circle);
 
     // Shape square(10, 5);
     // Circle circle(10);
